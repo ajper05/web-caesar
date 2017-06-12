d5085a841f19d4bda3c3a2726f4ec48e5af2319c
Anthony Perez <pokepics@hotmail.com>
2017-06-12 15:20:28 -0700

    code works now

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-from flask import Flask
-
+from flask import Flask, request
+from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
 form = """
-    <!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+    <form method="POST">
+                Rotate By: <input type="text" name="rot" value="0"><br>
+                <textarea name="text">{0}</textarea><br>
+                <input type ="submit" />
+    </form>
+    </body>
+</html>"""
 
-        <html>
-            <head>
-                <style>
-                    form {
-                        background-color: #eee;
-                        padding: 20px;
-                        margin: 0 auto;
-                        width: 540px;
-                        font: 16px sans-serif;
-                        border-radius: 10px;
-                    }
-                    textarea {
-                        margin: 10px 0;
-                        width: 540px;
-                        height: 120px;
-                    }
-                </style>
-            </head>
-            <body>
-                <form method="POST">
-                    Rotate By: <input type="text" name="rot" value="0"><br>
-                    <textarea name="text"></textarea>
-                </form>
-            </body>
-        </html>
-    """
 @app.route("/")
 def index():
-    return form
+    return form.format('')
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    """does the caesar thing"""
+    rotate = int(request.form['rot'])
+    rotatetext = str(request.form['text'])
+    finishedstr = rotate_string(rotatetext, rotate)
+    return "<h1>" + form.format(str(finishedstr)) + "</h1>"
+
 
 
 app.run()
